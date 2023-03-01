2e60a09c51f2dd22ff2b9ca8a239bdb55b851c1a
Chuong Nguyen <54233434+ChuongNg1996@users.noreply.github.com>
2023-03-01 10:28:16 +0700

    Add files via upload

--- Neural Network/FF_BP_one_hidden_var_neuron_1.m
+++ Neural Network/FF_BP_one_hidden_var_neuron_1.m
@@ -8,6 +8,9 @@
                         One feature input
                         One hidden layer with user defined number 
                         One output at the output layer
+                        
+                        If this is too cumbersome to read/understand, it is
+                        advised to go back and read FF_BP_simple_1.m 
 %}
 
 %{
@@ -55,14 +58,14 @@ datapt_num = length(data(:,1));         % Number of data points
 trainpt_num = round(datapt_num*ratio);  % Rounded number of training points
 
 % Training set
-x_train = x_data(1:trainpt_num);
-y_train = y_data(1:trainpt_num);
+x_train = x_data(1:trainpt_num);        % We want x_data*ratio
+y_train = y_data(1:trainpt_num);        % We want y_data*ratio
 % figure
 % plot(x_train,y_train);
 
 % Validation set
-x_valid = x_data(trainpt_num+1:end);
-y_valid = y_data(trainpt_num+1:end);
+x_valid = x_data(trainpt_num+1:end);    % We want x_data*(1-ratio)
+y_valid = y_data(trainpt_num+1:end);    % We want y_data*(1-ratio)
 % figure
 % plot(x_valid,y_valid);
 
@@ -70,15 +73,18 @@ y_valid = y_data(trainpt_num+1:end);
 
 %% Parameters
 
+% Ask user for desired number of neurons
 prompt = "Number of neurons? ";
 num_neuron = input(prompt);
 w_init = zeros(2*num_neuron,1);              % For 1 feature input and 1 output
 b_init = zeros(2,1);                         % For 1 feature input and 1 output
 
+% Ask user whether they want to manually input the weights & biases
 prompt = "Would you like to manually input the weight and bias? 1 is 'yes', 0 is 'no' ";
 manual_bool = input(prompt);
 
 if manual_bool == 1
+    % The case where user want to manually input
     for i = 1:length(w_init)
         prompt = strcat("Weight ",string(i)," is ");
         w_init(i) = input(prompt);
@@ -88,6 +94,8 @@ if manual_bool == 1
         b_init(i) = input(prompt);
     end
 else
+    % The case where user doesn't want to manually input
+    % Use default values for weights & biases
     w_default = 0.1;
     for i = 1:length(w_init)
         w_init(i) = w_default;
@@ -101,137 +109,108 @@ end
 
 % Add weight & bias to initial parameters, this step can be grouped with
 % above steps but is kept separated for clarity.
+
+% Arrange weights & biases as initial values of parameter array
+
+% The length of param_init is equal to sum length of both weights & biases
 param_init = zeros(length(w_init) + length(b_init),1);
-    for i = 1:length(w_init)
-        param_init(i) = w_init(i);
-    end
-    for i = 1:length(b_init)
-        param_init(length(w_init)+i) = b_init(i);
-    end
+% Assign weights
+for i = 1:length(w_init)
+    param_init(i) = w_init(i);
+end
+% Assign biases
+for i = 1:length(b_init)
+    % param_init picks up from last element of previous loop
+    param_init(length(w_init)+i) = b_init(i);
+end
     
 %% Back-propagation
 
 % Pre-calculation of derivative of gradient descent for all weight.
-syms x y out 
-w = sym('w',[1 length(w_init)]);
-b = sym('b',[1 length(b_init)]);
-zh = zeros(length(num_neuron),1);
-h = zeros(length(num_neuron),1);
-% % Input to hidden layer
-% zh1 = w1*x + b1;
-% zh2 = w2*x + b1;
-% zh3 = w3*x + b1;
-% % Activation func at hidden layer
-% h1 = sigmoid_act_func(zh1);
-% h2 = sigmoid_act_func(zh2);
-% h3 = sigmoid_act_func(zh3);
-% % Hidden layer to output layer
-% zo1 = w4*h1 + w5*h2 + w6*h3 + b2;
-% % Activation func at output layer
-% out = zo1*1.0;
-% 
-% % Cost function
-% err = (1/2)*(y-out)^2;
-% 
-% % Partial Derivative of cost function w.r.t. all weight and bias
-% err_w1_diff = diff(err,w1);
-% err_w2_diff = diff(err,w2);
-% err_w3_diff = diff(err,w3);
-% err_w4_diff = diff(err,w4);
-% err_w5_diff = diff(err,w5);
-% err_w6_diff = diff(err,w6);
-% err_b1_diff = diff(err,b1);
-% err_b2_diff = diff(err,b2);
-% 
-% %% Training Process
-% iteration_num = 1000;
-% learning_rate = 0.01;
-% 
-% param_old = param_init;
-% param_new = zeros(length(param_old),1);
-% 
-% for k = 1:iteration_num
-%     % w1
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w1_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(1) = param_old(1) - learning_rate*err_de;
-%     
-%     % w2
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w2_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(2) = param_old(2) - learning_rate*err_de;
-%     
-%     % w3
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w3_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(3) = param_old(3) - learning_rate*err_de;
-% 
-%     % w4
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w4_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(4) = param_old(4) - learning_rate*err_de;
-% 
-%     % w5
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w5_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(5) = param_old(5) - learning_rate*err_de;
-% 
-%     % w5
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_w6_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(6) = param_old(6) - learning_rate*err_de;
-% 
-%     % b1
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_b1_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(7) = param_old(7) - learning_rate*err_de;
-% 
-%     % b2
-%     err_de = 0;
-%     for i = 1:length(x_train)
-%         err_de = err_de + subs(err_b2_diff,[x,y,w1,w2,w3,w4,w5,w6,b1,b2],[x_train(i),y_train(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-%     end
-%     err_de = double(err_de/length(x_train));
-%     param_new(8) = param_old(8) - learning_rate*err_de;
-% 
-%     param_old = param_new;
-% end
-% 
-% %% Test Model(s)
-% y_new = zeros(length(y_data),1);
-% for i = 1:length(x_data)
-%     y_new(i) = subs(out,[x,w1,w2,w3,w4,w5,w6,b1,b2],[x_data(i),param_old(1),param_old(2),param_old(3),param_old(4),param_old(5),param_old(6),param_old(7),param_old(8)]);
-% end
-% figure
-% hold on
-% plot(x_data,y_data)
-% plot(x_data,y_new)
-% hold off
-% 
-% error = zeros(length(y_data),1);
-% for i = 1:length(x_data)
-%     error(i) = y_data(i) - y_new(i);
-% end
-% figure
-% plot(error)
+
+% Define symbolic variables
+syms x y out                        % feature input, measured output, output
+w = sym('w',[1 length(w_init)]);    % symbolic weight
+b = sym('b',[1 length(b_init)]);    % symbolic bias
+zh = sym('zh',[1 num_neuron]);      % symbolic input func to hidden layer
+h = sym('h',[1 num_neuron]);        % symbolic activation func
+
+% Input to hidden layer
+for i = 1:num_neuron
+    zh(i) = w(i)*x + b(1);
+    %  Activation func at hidden layer
+    h(i) = sigmoid_act_func(zh(i));
+end
+
+% Hidden layer to output layer
+zo1 = w(num_neuron+1)*h(1);
+for i = 2:num_neuron
+    zo1 = zo1 + w(num_neuron+i)*h(i);
+end
+zo1 = zo1 + b(2);
+% Activation func at output layer
+out = zo1*1.0;      % Use linear activation.
+
+% Cost function
+err = (1/2)*(y-out)^2;
+
+% Partial Derivative of Cost function w.r.t. all weights & biases
+err_diff = sym('err_diff', [1 length(param_init)]);
+% Partial Derivative of Cost function w.r.t. all weights
+for i = 1:length(w)
+    err_diff(i) = diff(err,w(i));
+end
+% Partial Derivative of Cost function w.r.t. all biases
+for i = 1:length(b)
+    err_diff(length(w)+i) = diff(err,b(i));
+end
+
+%% Training Process
+iteration_num = 1000;
+learning_rate = 0.01;
+
+param_old = param_init;
+% We want param_new to have length sum of weights & biases, so just use a
+% variable that have that, which is param_old in this case
+param_new = zeros(length(param_old),1); 
+
+for k = 1:iteration_num
+    
+    % Separate param_old into weights & biases to substitute to symbolic
+    % function. Also transpose them to match the array form.
+    w_param = param_old(1:length(w)); w_param = w_param';
+    b_param = param_old(length(w)+1:end); b_param = b_param';
+    
+    for i = 1:length(param_init)
+        err_de = 0;
+        for j = 1:length(x_train)
+            err_de = err_de + subs(err_diff(i),[x,y,w,b],[x_train(j),y_train(j),w_param,b_param]);
+        end
+        err_de = double(err_de)/length(x_train);
+        param_new(i) = param_old(i) - learning_rate*err_de;
+    end
+    param_old = param_new;
+end
+
+%% Test Model(s)
+y_new = zeros(length(y_data),1);
+
+% Separate param_old into weights & biases to substitute to symbolic
+% function. Also transpose them to match the array form.
+w_param = param_new(1:length(w)); w_param = w_param';
+b_param = param_new(length(w)+1:end); b_param = b_param';
+for i = 1:length(x_data)
+    y_new(i) = subs(out,[x,w,b],[x_data(i),w_param,b_param]);
+end
+figure
+hold on
+plot(x_data,y_data)
+plot(x_data,y_new)
+hold off
+
+error = zeros(length(y_data),1);
+for i = 1:length(x_data)
+    error(i) = y_data(i) - y_new(i);
+end
+figure
+plot(error)
